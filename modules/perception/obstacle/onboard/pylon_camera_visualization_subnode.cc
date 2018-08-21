24992c07b54650a9b0cc81de72d3bfe5cf424f5c
GPrathap <ggeesara@gmail.com>
2018-08-21 23:15:56 +0300

    adding a testing config for front camera

--- modules/perception/obstacle/onboard/pylon_camera_visualization_subnode.cc
+++ modules/perception/obstacle/onboard/pylon_camera_visualization_subnode.cc
@@ -114,18 +114,14 @@ bool TrackVisualizationSubnode::InitInternal() {
       AINFO << "Init shared camera_shared_right_data_ successfully, data: "
             << camera_shared_right_data_->name();
     }else if( reserve_field_map["camera_orientation"] == "left"){
-      AERROR << "----|1--->>>";
       camera_object_left_data_ = static_cast<PylonCameraLeftObjectData *>(
               shared_data_manager_->GetSharedData("PylonCameraLeftObjectData"));
-      AERROR << "----|2--->>>";
       camera_shared_left_data_ = static_cast<PylonCameraLeftSharedData *>(
               shared_data_manager_->GetSharedData("PylonCameraLeftSharedData"));
-      AERROR << "----|3--->>>";
       if (camera_object_left_data_ == nullptr) {
         AERROR << "Failed to get  camera_object_left_data_ object.";
         return false;
       }
-      AERROR << "----|4--->>>";
       AINFO << "Init object camera_shared_left_data_ successfully, data: "
             << camera_object_left_data_->name();
 
@@ -176,7 +172,7 @@ bool TrackVisualizationSubnode::InitInternal() {
               shared_data_manager_->GetSharedData("PylonCameraObjectData"));
       cam_shared_data_ = static_cast<PylonCameraSharedData *>(
               shared_data_manager_->GetSharedData("PylonCameraSharedData"));
-      if (camera_object_left_backwards_side_data_ == nullptr) {
+      if (cam_obj_data_ == nullptr) {
         AERROR << "Failed to get  cam_obj_data_ object.";
         return false;
       }
