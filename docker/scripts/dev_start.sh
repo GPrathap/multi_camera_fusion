420fd35909c63c0df61796669ddf750322aa1b3d
GPrathap <ggeesara@gmail.com>
2018-08-23 17:06:56 +0300

    adding config

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -256,6 +256,14 @@ function main(){
     docker pull ${YOLO3D_VOLUME_IMAGE}
     docker run -it -d --rm --name ${YOLO3D_VOLUME} ${YOLO3D_VOLUME_IMAGE}
 
+    XAUTH=/tmp/.docker.xauth
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | sudo xauth -f $XAUTH nmerge -
+    sudo chmod 777 $XAUTH
+    X11PORT=`echo $DISPLAY | sed 's/^[^:]*:\([^\.]\+\).*/\1/'`
+    TCPPORT=`expr 6000 + $X11PORT`
+    sudo ufw allow from 172.17.0.0/16 to any port $TCPPORT proto tcp
+    DISPLAY=`echo $DISPLAY | sed 's/^[^:]*\(.*\)/172.17.0.1\1/'`
+
     info "Starting docker container \"apollo_dev\" ..."
     docker run -it \
         -d \
@@ -272,6 +280,8 @@ function main(){
         -e DOCKER_GRP="$GRP" \
         -e DOCKER_GRP_ID=$GRP_ID \
         -e DOCKER_IMG=$IMG \
+        -v $XAUTH:$XAUTH \
+        -e XAUTHORITY=$XAUTH \
         $(local_volumes) \
         -w /apollo \
         --add-host in_dev_docker:127.0.0.1 \
