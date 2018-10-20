784d30071d50aa7c07d3c65933f937917f486a57
GPrathap <ggeesara@gmail.com>
2018-10-20 16:59:49 +0300

    fixing a typo

--- modules/perception/obstacle/onboard/radar_process_subnode.cc
+++ modules/perception/obstacle/onboard/radar_process_subnode.cc
@@ -157,7 +157,7 @@ void RadarProcessSubnode::OnRadar(const ContiRadar &radar_obs) {
         Singleton<CalibrationConfigManager>::get();
     //TODO for the now it uses default camera if camera id is not provided
     calibration_config_manager->set_device_id_and_calibration_config_manager_init("");
-    CameraCalibrationPtr calibrator = config_manager->get_camera_calibration();
+    CameraCalibrationPtr calibrator = calibration_config_manager->get_camera_calibration();
     // Eigen::Matrix4d camera_to_car = calibrator->get_camera_extrinsics();
     *radar2car_pose = radar_extrinsic_;
     ADEBUG << "get radar trans pose succ. pose: \n" << *radar2car_pose;
