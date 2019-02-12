56aa7fc5be239cf63af702d0aae8191f3ee4801f
Aidar Gabdullin <a.gabdullin@innopolis.ru>
2019-02-12 13:14:29 +0300

    map offset works

--- modules/dreamview/frontend/src/components/MapOffset/index.js
+++ modules/dreamview/frontend/src/components/MapOffset/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { MAP_WS } from "store/websocket";
 
+const IncrementStep = 0.000001;
+
 export default class MapOffset extends React.PureComponent {
     constructor(){
         super();
@@ -9,14 +11,53 @@ export default class MapOffset extends React.PureComponent {
           lon: 1
         };
         this.handleChange = this.handleChange.bind(this);
+        this.decLat = this.decLat.bind(this);
+        this.incLat = this.incLat.bind(this);
+        this.decLon = this.decLon.bind(this);
+        this.incLon = this.incLon.bind(this);
     }
 
     handleChange (evt) {
-        this.setState({ [evt.target.name]: evt.target.value });
+        this.setState({ [evt.target.name]: parseFloat(evt.target.value) });
     }
 
     setValues = evt => {
-        MAP_WS.changeMapOffset(this.state);
+        //this routine is done due to keep
+        //state var as floats, but send as string (crazy)
+        const state={
+            lat: this.state.lat+'',
+            lon: this.state.lon+''
+        };
+        MAP_WS.changeMapOffset(state);
+    }
+
+    decLat() {
+        this.setState(lat => ({
+            lat: this.state.lat - IncrementStep,
+        }));
+        this.setValues();
+      }
+
+    incLat() {
+        this.setState(lat => ({
+            lat: this.state.lat + IncrementStep,
+        }));
+        this.setValues();
+    }
+
+    decLon() {
+        this.setState(lat => ({
+            lon: this.state.lon - IncrementStep,
+        }));
+        this.setValues();
+      }
+
+
+    incLon() {
+        this.setState(lat => ({
+            lon: this.state.lon + IncrementStep,
+        }));
+        this.setValues();
     }
 
     render() {
@@ -25,15 +66,19 @@ export default class MapOffset extends React.PureComponent {
                 <div className="offset-window">
                     <div className="text">
                         <div className="label">lat: </div>
-                        <input type="text" name="lat"
+                        <input type="text" name="lat" tabindex="1"
                     onChange={this.handleChange} value={this.state.lat} />
+                        <button onClick={this.incLat}>+</button>
+                        <button onClick={this.decLat}>-</button>
                     </div>
                     <div className="text">
                         <div className="label">lon: </div>
-                        <input type="text" name="lon"
+                        <input type="text" name="lon" tabindex="2"
                     onChange={this.handleChange} value={this.state.lon} />
+                        <button onClick={this.incLon}>+</button>
+                        <button onClick={this.decLon}>-</button>
                     </div>
-                    <button onClick={this.setValues}>Set!</button>
+                    <button onClick={this.setValues} tabindex="3">Set!</button>
                 </div>
             </div>
         );
